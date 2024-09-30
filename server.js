505fcc78e40047f9811ce3effed12009907912aa
Nidhi-hub-pixel <bhanu.131984@gmail.com>
2024-09-30 16:23:19 +0530

    hello

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+const { v4: uuidv4 } = require("uuid");
+const io=require("socket.io")(server,{
+    cors:{
+        origin:'*'
+    }
+})
+
+const {ExpressPeerServer}=require("peer")
+const peerServer=ExpressPeerServer(server,{debug:true})
+
+app.use("/peerjs",peerServer)
+app.get("/", (req, res) => {
+    res.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:room", (req, res) => {
+    res.render("index", { roomId: req.params.room });
+});
+io.on("connection",(socket)=>{
+    socket.on("join-room",(roomId,userId,userName)=>{
+        socket.join(roomId)
+    
+    socket.on("message",(message)=>{
+        io.to(roomId).emit("createMessage",message,userName)
+    })
+})
+})
+server.listen(3030);
+
+
+//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
